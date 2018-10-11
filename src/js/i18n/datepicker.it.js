cb5c22fdab48f2020a9aaf0b981482cf3f5aca78
aminta <davide.lorigliola@gmail.com>
2018-10-12 00:02:51 +0200

    Short names raised to 3 chars

--- src/js/i18n/datepicker.it.js
+++ src/js/i18n/datepicker.it.js
@@ -1,7 +1,7 @@
 ;(function ($) { $.fn.datepicker.language['it'] = {
     days: ['Domenica', 'Lunedi', 'Martedi', 'Mercoledi', 'Giovedi', 'Venerdi', 'Sabato'],
     daysShort: ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'],
-    daysMin: ['Do', 'Lu', 'Ma', 'Me', 'Gi', 'Ve', 'Sa'],
+    daysMin: ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'],
     months: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
     monthsShort: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
     today: 'Oggi',
